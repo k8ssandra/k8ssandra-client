f37b3343d3fe5799895c26b5c1e2608b1961de1c
Michael Burman <yak@iki.fi>
2024-09-16 14:38:46 +0300

    Add jvm.options as copy-file also

--- pkg/config/builder.go
+++ pkg/config/builder.go
@@ -511,7 +511,7 @@ func writeYaml(doc map[string]interface{}, targetFile string) error {
 
 func copyFiles(sourceDir, targetDir string) error {
 	// Copy the files we're not modifying
-	files := []string{"jvm-clients.options", "jvm11-clients.options", "jvm17-clients.options", "logback.xml", "logback-tools.xml", "jvm-dependent.sh"}
+	files := []string{"jvm-clients.options", "jvm11-clients.options", "jvm17-clients.options", "logback.xml", "logback-tools.xml", "jvm-dependent.sh", "jvm.options"}
 
 	for _, f := range files {
 		sourceFile := filepath.Join(sourceDir, f)
