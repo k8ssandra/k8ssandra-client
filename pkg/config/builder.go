87cd0de436308edd6388710850983789048b7953
Rahul Shukla <shukla2009@gmail.com>
2025-01-30 14:32:18 +0800

    add cassandra-jaas.config file to be copied to /config folder

--- pkg/config/builder.go
+++ pkg/config/builder.go
@@ -504,7 +504,7 @@ func writeYaml(doc map[string]interface{}, targetFile string) error {
 
 func copyFiles(sourceDir, targetDir string) error {
 	// Copy the files we're not modifying
-	files := []string{"jvm-clients.options", "jvm11-clients.options", "jvm17-clients.options", "logback.xml", "logback-tools.xml"}
+	files := []string{"jvm-clients.options", "jvm11-clients.options", "jvm17-clients.options", "logback.xml", "logback-tools.xml", "cassandra-jass.config"}
 
 	for _, f := range files {
 		sourceFile := filepath.Join(sourceDir, f)
